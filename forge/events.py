3d0ae749bb0e4ad84e88fb06896c2232ae238866
allwzsmokin1 <allwzsmokin@gmail.com>
2026-07-28 18:12:34 -0400

    Implement EventBus class for event handling

---
+++ forge/events.py
@@ -0,0 +1,17 @@
+from collections import defaultdict
+from typing import Callable, Any
+
+
+class EventBus:
+    def __init__(self):
+        self._listeners = defaultdict(list)
+
+    def subscribe(self, event: str, callback: Callable):
+        self._listeners[event].append(callback)
+
+    def publish(self, event: str, data: Any = None):
+        for callback in self._listeners[event]:
+            callback(data)
+
+
+bus = EventBus()
